73b766fc6c1fa34e6cff487e4455864186624e61
mateus-bisognin <42121959+mateus-bisognin@users.noreply.github.com>
2019-10-31 14:40:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res) {
-    res.sendFile('./index.html')
+    res.sendFile('views/index.html', {root: __dirname })
 });
 
 
